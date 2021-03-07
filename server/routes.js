a75e2737a82bfef78ea3a7daf23a3e520931a57e
Ubuntu <azureuser@roohanjyot.1qffnhncemrelmggdn2nzorldd.xx.internal.cloudapp.net>
2021-03-07 03:58:08 +0000

    Day 2 - implimenting the url to say domain name instead of the ip address

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+// Requiring Libs
+const router = require("express").Router();
+
+// Establishing Call Architecture
+// router.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+module.exports = router;
